0b36e3befb0d40bc96595e2b05f09069e9c8adad
Abhi Tripathi <abztrypath@Abhis-MacBook-Air.local>
2025-03-06 15:32:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>वेदिक उद्धरण ऐप</h1>
+      <p>यहां आपको रोज़ नए वेदों और गीता के उद्धरण मिलेंगे।</p>
+    </div>
+  );
+}
+
+export default App;
+
